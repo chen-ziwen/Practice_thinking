93708fb9b83c0e4071b2e8bf30c6882c13c24966
Chiko <2452559902@qq.com>
2023-06-14 21:46:10 +0800

    add: 深拷贝

---
+++ API封装/深拷贝.js
@@ -0,0 +1,17 @@
+// 虽然说 JSON.parse(JSON.stringify(obj)) 可以实现大部分对象的深拷贝 但是面对一个特殊对象 例如函数，Date对象，Ma对象，Set对象就无法进行拷贝
+
+function deepClone(data) {
+    if (typeof data !== 'object' || data === null) {
+        return data;
+    }
+
+    const clone = Array.isArray(data) ? [] : {};
+
+    for (let key in data) {
+        if (data.hasOwnProperty(key)) {
+            clone[key] = deepClone(data[key]);
+        }
+    }
+
+    return clone;
+}
