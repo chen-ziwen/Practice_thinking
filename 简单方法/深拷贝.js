b3710f3916b64136041cec2b43248161a4a65f93
chenziwen <chenziwen@qesong.com>
2023-06-16 15:45:23 +0800

    add: 简单的深拷贝

---
+++ 简单方法/深拷贝.js
@@ -0,0 +1,26 @@
+
+// 实现数组，对象的深拷贝
+function deepClone(obj) {
+    if (typeof obj !== "object" || obj === null) {
+        return obj;
+    }
+    const data = Array.isArray(obj) ? [] : {};
+    for (let key in obj) {
+        data[key] = deepClone(obj[key]);
+    }
+    return data;
+}
+
+const origin = {
+    name: 'czw',
+    age: 23,
+    type: ['normal', 'del', 'add'],
+}
+
+const b = origin;
+const c = deepClone(origin);
+
+console.log(b === origin,b); //  true { name: 'czw', age: 23, type: [ 'normal', 'del', 'add' ] }
+console.log(b.type === origin.type,b.type); // true ['normal', 'del', 'add']
+console.log(c === origin, c); // false  { name: 'czw', age: 23, type: [ 'normal', 'del', 'add' ] }
+console.log(c.type === origin.type, c.type); // false ['normal', 'del', 'add']
