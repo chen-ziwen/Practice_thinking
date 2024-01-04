cba23ce6a2396a6a4d1040b844fbaa26689355f5
chenziwen <chenziwen@qesong.com>
2024-01-04 14:50:55 +0800

    add: 一点类的扩展思路

---
+++ 设计模式/driver.js
@@ -0,0 +1,31 @@
+
+// class 的其他用法 类并不是说只有实例化后才能保存数据
+// 在这个案例中 类也可以向对象一样使用 虽然它并没有实例化 但是改变内部的数据也是有效的 和对象表现一致
+
+class Driver {
+    static mDriver = {};
+    static use(name, value) {
+        if (this.has(name)) {
+            console.log("重复了");
+        } else {
+            this.mDriver[name] = value;
+        }
+    }
+
+    static create() {
+        console.log('输出driver', this.mDriver);
+    }
+
+
+    static has(name) {
+        return this.mDriver[name] != null;
+    }
+}
+
+Driver.use("name", "chiko");
+Driver.use("age", 22);
+Driver.use("sex", "男");
+Driver.use("isStudent", false);
+
+
+Driver.create(); // 输出driver { name: 'chiko', age: 22, sex: '男', isStudent: false }
