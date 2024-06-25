96262c75c1db2f36a27daa24fa146d580efd6b85
Chiko <2452559902@qq.com>
2024-06-25 13:58:36 +0800

    add: 增加无限滚动动画

--- 功能函数/实现new.js
+++ 功能函数/实现new.js
@@ -8,7 +8,7 @@ function myNew(constructor, ...args) {
     const obj = Object.create(constructor.prototype); // 创建一个指定原型的对象
     // 2. 执行构造函数，将this指向新创建的对象
     const result = constructor.apply(obj, args);
-    // 3. 如果构造函数返回一个对象，则返回这个对象；否则返回新创建的对象 
+    // 3. 如果构造函数返回一个对象，则返回这个对象；否则返回新创建的对象 (这是js的构造函数的规定)
     return result && typeof result === "object" ? result : obj;
 
 }
